6b36e0771a66b471077c26e41a43f091c9471900
pevevoblip <romankapitan9@gmail.com>
2024-11-02 19:01:49 +0200

    Les 1-2

---
+++ Main.py
@@ -0,0 +1,5 @@
+from Student import Student
+student = Student("Ivan, 15, True")
+student1 = Student("Roman, 15, True")
+print(f"Name: {student.Name}\nAge: {student.Age}\nSex: {student.Sex}")
+print(f"Name: {student1.Name}\nAge: {student1.Age}\nSex: {student1.Sex}")
---
+++ Student.py
@@ -0,0 +1,5 @@
+class Student:
+     def __init__(self, name : str = "Test", age: float = 0, sex: bool=False):
+         self.Name = name
+         self.Age = age
+         self.Sex = sex

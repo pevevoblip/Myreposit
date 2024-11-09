6ecd74357ac4a11247e64190fbf1a95471006243
pevevoblip <romankapitan9@gmail.com>
2024-11-09 11:23:22 +0200

    Les 1-2

---
+++ Dog.py
@@ -0,0 +1,6 @@
+class Dog:
+     def __init__(self, name : str = "Sousege", age: float = 7, sex: bool=True, breed : str = "dachshund"):
+         self.Name = name
+         self.Age = age
+         self.Sex = sex
+         self.Breed = breed
--- Main.py
+++ Main.py
@@ -1,5 +1,4 @@
-from Student import Student
-student = Student("Ivan, 15, True")
-student1 = Student("Roman, 15, True")
-print(f"Name: {student.Name}\nAge: {student.Age}\nSex: {student.Sex}")
-print(f"Name: {student1.Name}\nAge: {student1.Age}\nSex: {student1.Sex}")
+from Dog import Dog
+dog = Dog("Sousege")
+
+print(f"Name: {dog.Name}\nAge: {dog.Age}\nSex: {dog.Sex}\nBreed: {dog.Breed}:")
